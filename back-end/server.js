931a98a6bd49b8fc1c56cff9037a152fe6a3e18c
trl6895 <trl6895@g.rit.edu>
2022-08-09 11:31:05 -0400

    further progress on the backend. can now query games db. requires that db is online before running

--- back-end/server.js
+++ back-end/server.js
@@ -1,20 +1,7 @@
-/*import mongoose from "mongoose";
 
-export async function mongoSetup() {
-    const mongoose = require('mongoose');
-    try {
-        await mongoose.connect('mongodb://localhost:27017/mocksteam', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-          });
-    } catch (error) {
-        console.log(error);
-    }
-    console.log("mongodb setup done");
-}*/
 
 var express = require('express');
+//const { default: mongoose } = require('mongoose');
 var app = express();
 
 app.get('/', function(req, res) {
@@ -24,3 +11,80 @@ app.get('/', function(req, res) {
 var server = app.listen(8080, function() {
     console.log("Backend Application listening at http://localhost:8080")
 })
+
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/mocksteam');
+
+    const gameSchema = new mongoose.Schema({
+        img_url: String,
+        date: String,
+        developer: String,
+        publisher: String,
+        full_desc: Object,
+        requirements: Object,
+        popu_tags: [String],
+        price: Number,
+        url_info: Object,
+        name: { type: String,
+            required: true },
+        categories: [String]
+    },
+    {
+        collection: "games"
+    });
+
+    const Game = new mongoose.model('games', gameSchema);
+    /*
+    const chic = new Game({ name: 'Chicory: A Colorful Tale'});
+    console.log(chic.name);
+
+    await chic.save();
+    */
+    //console.log(await Game.find({$or: [{name: 'Among Us'}, {publisher: /Annapurna.*/}]}, { name: 1, developer: 1, publisher: 1, _id: 0 }));
+    
+        /*
+    const kittySchema = new mongoose.Schema({
+        name: String
+    });
+
+    const Kitten = mongoose.model('Kitten', kittySchema);
+
+    const silence = new Kitten({ name: 'Silence' });
+    console.log(silence.name);
+
+    await silence.save();
+    */
+}
+/*
+async () => {
+    const mongoose = require('mongoose');
+    try {
+        await mongoose.connect('mongodb://localhost:27017/', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          });
+    } catch (error) {
+        console.log(error);
+    }
+    console.log("mongodb setup done");
+}
+*/
+//var db = mongoose.connection;
+//db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+//const connection = mongoose.connection;
+//const steamdb = connection.useDb('mocksteam');
+//console.log(steamdb);
+
+
+
+async function queryFunc (query) {
+    console.log(await query.exec());
+}
+
+//queryFunc(Game.findOne({ name: 'Among Us'}));
